ed243a2e6c1e4decc17cc232d131bdd24bfe367a
Samir Gautam <samirgautam269@gmail.com>
2021-09-12 10:14:17 +0545

    first commit

---
+++ CRUDApp/Models/AdbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUDApp.Models
+{
+    public class AdbContext : IdentityDbContext
+    {
+        public AdbContext(DbContextOptions<AdbContext> options)
+            : base(options)
+        { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+        }
+
+        public DbSet<Product> Product { get; set; }
+    }
+}
